0503e4a2a27bb712e17f2be14f9a626e04e30167
IrynaSss <irynasosnich@gmail.com>
2021-01-26 13:22:19 -0600

    started working on feature A

---
+++ src/test/java/FeatureACode.java
@@ -0,0 +1,5 @@
+public class FeatureACode {
+    public static void main(String[] args) {
+        System.out.println("Work on A started");
+    }
+}
--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -1,3 +1,6 @@
 public class HelloTest {
     //testing my hello
+
+
+
 }
