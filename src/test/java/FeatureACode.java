ab1dfb53c88e02532efe2997140423aa3c1d0a6c
IrynaSss <irynasosnich@gmail.com>
2021-01-26 13:23:31 -0600

    more work on feature A

--- src/test/java/FeatureACode.java
+++ src/test/java/FeatureACode.java
@@ -1,5 +1,6 @@
 public class FeatureACode {
     public static void main(String[] args) {
         System.out.println("Work on A started");
+        System.out.println("B21 is learning more git with review");
     }
 }
